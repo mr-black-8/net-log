324c537fa8ab8e0bd171b9220ecb9f83b31f4d87
mr-black-8 <boz.black@gmail.com>
2018-07-21 17:51:14 +1000

    adding network connection test & fixing ScheduledTaskTrigger

--- create.ps1
+++ create.ps1
@@ -1,27 +1,44 @@
 param(
-  [Int32]$TestInterval = 30,
-  [String]$LogPath = "$home\net-log"
+  [Int32]$TestInterval = 1, #minutes
+  [String]$LogPath = "C:\net-log"
 )
 
 $Command = '$ErrorActionPreference = "Stop";
+$NetAdaptors = Get-NetAdapter;
+$HasNetworkConnection = $false;
+foreach ($Adaptor in $NetAdaptors) {
+  if ($Adaptor.status -eq "Up") {
+    $HasNetworkConnection = $true;
+  }
+}
+
+If(!(Test-Path $LogPath)) {
+  New-Item -ItemType Directory -Path $LogPath -Force;
+}
+
+if ($HasNetworkConnection) {
   try {
     Test-Connection -Count 1 -ComputerName 8.8.8.8 | Out-Null;
   } catch {
-    If(!(test-path $LogPath)) {
-      New-Item -ItemType Directory -Path $LogPath -Force;
+    try {
+      Test-Connection -Count 1 -ComputerName 1.1.1.1 | Out-Null;
+    } catch {
+      $Msg = "$((Get-Date).ToString()),1`n";
+      $LogPath = "$LogPath\$(Get-Date -UFormat %Y-%m).csv";
+      Out-File -FilePath $LogPath -InputObject $Msg -Append;
     }
-
-    $Msg = "$((Get-Date).ToString()),1`n";
-    $LogPath = "$LogPath\$(Get-Date -UFormat %Y-%m).csv";
-    Out-File -FilePath $LogPath -InputObject $Msg -Append;
   }
+} else {
+  $Msg = "$((Get-Date).ToString()),2`n";
+  $LogPath = "$LogPath\$(Get-Date -UFormat %Y-%m).csv";
+  Out-File -FilePath $LogPath -InputObject $Msg -Append;
 }'
 
 $TaskAction = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -WindowStyle Hidden -command $Command"
-$TaskTrigger = New-ScheduledTaskTrigger -RepititionInterval "${$TestInterval}s"
+$TaskTrigger = New-ScheduledTaskTrigger -Once -At(Get-Date) -RepititionInterval (New-TimeSpan -Minutes $TestInterval);
 
 $ExistingTask = Get-ScheduledTask -TaskName "Net-Log"
-if ($ExistingTask -ne $null) {
+if (!$ExistingTask) {
   Set-ScheduledTask "Net-Log" -Action $TaskAction -Trigger $TaskTrigger
 } else {
   Register-ScheduledTask -Action $TaskAction -Trigger $TaskTrigger -TaskName "Net-Log" -Description "Checks for & logs internet outages"
