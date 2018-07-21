621ab774a9bc3b47fe30bcf731052eeb39f944d9
mr-black-8 <boz.black@gmail.com>
2018-07-21 20:13:30 +1000

    adding principal to task & fixing escape chars in command string

--- create.ps1
+++ create.ps1
@@ -3,17 +3,17 @@ param(
   [String]$LogPath = "C:\net-log"
 )
 
-$Command = '$ErrorActionPreference = "Stop";
+$Command = '$ErrorActionPreference = ''Stop'';
 $NetAdaptors = Get-NetAdapter;
 $HasNetworkConnection = $false;
 foreach ($Adaptor in $NetAdaptors) {
-  if ($Adaptor.status -eq "Up") {
+  if ($Adaptor.status -eq ''Up'') {
     $HasNetworkConnection = $true;
   }
 }
 
-If(!(Test-Path $LogPath)) {
-  New-Item -ItemType Directory -Path $LogPath -Force;
+If(!(Test-Path '+$LogPath+')) {
+  New-Item -ItemType Directory -Path '+$LogPath+' -Force;
 }
 
 if ($HasNetworkConnection) {
@@ -23,23 +23,23 @@ if ($HasNetworkConnection) {
     try {
       Test-Connection -Count 1 -ComputerName 1.1.1.1 | Out-Null;
     } catch {
-      $Msg = "$((Get-Date).ToString()),1`n";
-      $LogPath = "$LogPath\$(Get-Date -UFormat %Y-%m).csv";
-      Out-File -FilePath $LogPath -InputObject $Msg -Append;
+      $Msg = (Get-Date).ToString() + '',1'';
+      $Path = '''+$LogPath+'\''+ (Get-Date -UFormat %Y-%m) + ''.csv'';
+      Out-File -FilePath $Path -InputObject $Msg -Append;
     }
   }
 } else {
-  $Msg = "$((Get-Date).ToString()),2`n";
-  $LogPath = "$LogPath\$(Get-Date -UFormat %Y-%m).csv";
-  Out-File -FilePath $LogPath -InputObject $Msg -Append;
+  $Msg = (Get-Date).ToString() + '',2'';
+  $Path = '''+$LogPath+'\''+ (Get-Date -UFormat %Y-%m) + ''.csv'';
+  Out-File -FilePath $Path -InputObject $Msg -Append;
 }'
 
-$TaskAction = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -WindowStyle Hidden -command $Command"
-$TaskTrigger = New-ScheduledTaskTrigger -Once -At(Get-Date) -RepititionInterval (New-TimeSpan -Minutes $TestInterval);
+$TaskAction = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -WindowStyle Hidden -command $Command";
+$TaskTrigger = New-ScheduledTaskTrigger -Once -At(Get-Date) -RepetitionInterval (New-TimeSpan -Minutes $TestInterval);
+$TaskPrincipal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest;
 
-$ExistingTask = Get-ScheduledTask -TaskName "Net-Log"
-if (!$ExistingTask) {
-  Set-ScheduledTask "Net-Log" -Action $TaskAction -Trigger $TaskTrigger
+if (Get-ScheduledTask -TaskName "Net-Log" -ErrorAction SilentlyContinue) {
+  Set-ScheduledTask "Net-Log" -Action $TaskAction -Trigger $TaskTrigger;
 } else {
-  Register-ScheduledTask -Action $TaskAction -Trigger $TaskTrigger -TaskName "Net-Log" -Description "Checks for & logs internet outages"
+  Register-ScheduledTask -Principal $TaskPrincipal -Action $TaskAction -Trigger $TaskTrigger -TaskName "Net-Log" -Description "Checks for & logs internet outages";
 }
